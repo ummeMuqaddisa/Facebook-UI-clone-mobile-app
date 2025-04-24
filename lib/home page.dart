b7655ef2b12d465619964d7b4cea9ed8748a712c
Umme Muqaddisa <muqaddisaumme@gmail.com>
2025-04-24 10:41:13 +0600

    .

---
+++ lib/home page.dart
@@ -0,0 +1,196 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class homepage extends StatelessWidget {
+  const homepage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController(length: 6,
+        child: Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        toolbarHeight: 50,
+        backgroundColor: Colors.white ,
+        title: Text("facebook",style: TextStyle(fontSize: 30,fontWeight: FontWeight.bold,color: Colors.blue[700]),),
+        actions: [
+          IconButton(onPressed: (){}, icon: Icon(Icons.add_circle)),
+          IconButton(onPressed: (){}, icon: Icon(Icons.search)),
+          IconButton(onPressed: (){}, icon: Icon(Icons.mode_comment)),
+
+        ],
+        bottom: TabBar(
+            indicatorColor:Colors.blue[700],
+            indicatorSize: TabBarIndicatorSize.tab,
+            labelColor: Colors.blue[700],
+            unselectedLabelColor: Colors.black87,
+            tabs: [
+          Tab(
+              icon: Icon(Icons.home_filled,size: 28,)
+          ),
+          Tab(
+              icon: Icon(Icons.ondemand_video_outlined,size:28)
+          ),
+          Tab(
+              icon: Icon(Icons.storefront,size:28)
+          ),
+          Tab(
+              icon: Icon(Icons.videogame_asset_outlined,size:28)
+          ),
+
+          Tab(
+              icon: Icon(Icons.notifications_none,size:28)
+          ),
+          Tab(
+              icon: Icon(Icons.menu,size:28)
+          ),
+        ]),
+      ),
+
+          body:
+            TabBarView(children: [
+              Container(color: Colors.white,
+                child: Column(
+                  children: [
+                    Container(
+
+                      height: 60,
+                      child:
+                        Row(children: [
+                          Container(width:8,),
+                          CircleAvatar(
+                            backgroundImage: NetworkImage("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQRXxfn1j1vKFy8yJeBGl2AS6Dcah-lKgHofg&s"),
+                            child: Icon(Icons.person),
+                            radius:25,
+                          ),
+                          Container(width: 12,),
+                          Container(
+                            margin: EdgeInsets.only(top: 10,bottom: 10),
+                            width: 280,
+                            //color: Colors.green,
+                            child: TextField(
+
+                              decoration: InputDecoration(
+                                contentPadding: EdgeInsets.only(top: 10,bottom: 10,left: 23),
+                                  hintText: "What's on your mind?",
+                                  border: OutlineInputBorder(borderRadius: BorderRadius.circular(60),borderSide: BorderSide(color: Colors.grey,width: 0.8) ),
+
+
+                              ),
+
+
+
+                            ),
+
+                          ),
+                          Container(width: 17,),
+                          Container(
+                            child:
+                            Icon(Icons.photo_rounded,color: Colors.green[600]),
+
+                          )
+
+                        ],)
+                    ),
+                    Container(
+                      color: Colors.grey[350],
+                      height: 4,
+                    ),
+
+                    Container(
+
+                      color: Colors.white,
+                      height: 220,
+                      child:
+                      SingleChildScrollView(
+                        scrollDirection: Axis.horizontal,
+                        child: Row(
+                          children: [
+                          Container(
+                            margin: EdgeInsets.only(left: 8),
+                            height: 200, width: 120,
+                            decoration: BoxDecoration(borderRadius: BorderRadius.circular(15),color: Colors.white10),
+                            child: Column(
+                              children: [
+                                Container(
+                                  height: 140, width: double.infinity,
+                                  decoration: BoxDecoration(
+                                    borderRadius: BorderRadius.only(topLeft:Radius.circular(15),topRight: Radius.circular(15) ),
+                                      image: DecorationImage(fit: BoxFit.cover,image: NetworkImage( "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQRXxfn1j1vKFy8yJeBGl2AS6Dcah-lKgHofg&s",),)),
+                                ),
+
+                                Container(
+                                  height: 60, width: double.infinity,
+                                  decoration: BoxDecoration(
+                                      borderRadius: BorderRadius.only(topLeft:Radius.circular(15),topRight: Radius.circular(15) ),
+                                      
+                                ),
+                                  child:
+
+                                  Center(child: Text("Create \n stroy")),
+                                    
+                                )
+                              ],
+                            ),
+                          ),
+
+                            Container(
+                              margin: EdgeInsets.only(left: 8),
+                              height: 200, width: 120,
+                              decoration: BoxDecoration(borderRadius: BorderRadius.circular(15),color: Colors.red),
+
+                            ),
+                            Container(
+                              margin: EdgeInsets.only(left: 8),
+                              height: 200, width: 120,
+                              decoration: BoxDecoration(borderRadius: BorderRadius.circular(15),color: Colors.blue),
+
+                            ),
+                            Container(
+                              margin: EdgeInsets.only(left: 8),
+                              height: 200, width: 120,
+                              decoration: BoxDecoration(borderRadius: BorderRadius.circular(15),color: Colors.grey),
+
+                            ),
+                            Container(
+                              margin: EdgeInsets.only(left: 8),
+                              height: 200, width: 120,
+                              decoration: BoxDecoration(borderRadius: BorderRadius.circular(15),color: Colors.pink),
+
+                            ),
+                            Container(
+                              margin: EdgeInsets.only(left: 8),
+                              height: 200, width: 120,
+                              decoration: BoxDecoration(borderRadius: BorderRadius.circular(15),color: Colors.blueGrey),
+
+                            ),
+
+
+
+
+                          ],),
+                      ),
+                    ),
+                    Container(
+                      color: Colors.grey[350],
+                      height: 4,
+                    ),
+
+                  ],
+                ),
+
+              ),
+              Container(color: Colors.black),
+              Container(color: Colors.white),
+              Container(color: Colors.white),
+              Container(color: Colors.white),
+              Container(color: Colors.white),
+
+            ])
+
+
+
+    ));
+
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/scheduler.dart';
 
+import 'home page.dart';
+
 void main() {
   runApp(const MyApp());
 }
@@ -12,136 +14,145 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-
+     debugShowCheckedModeBanner: false,
+    //  home: login(),
       home: homepage(),
     );
   }
 }
-
-class homepage extends StatelessWidget {
-  const homepage({super.key});
+class login extends StatelessWidget {
+  const login({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return DefaultTabController(length: 6,
-        child: Scaffold(
+    TextEditingController username= TextEditingController();
+    TextEditingController password= TextEditingController();
+    return Scaffold(
       backgroundColor: Colors.white,
       appBar: AppBar(
-        toolbarHeight: 50,
-        backgroundColor: Colors.white ,
-        title: Text("facebook",style: TextStyle(fontSize: 30,fontWeight: FontWeight.bold,color: Colors.blue[700]),),
-        actions: [
-          IconButton(onPressed: (){}, icon: Icon(Icons.add_circle)),
-          IconButton(onPressed: (){}, icon: Icon(Icons.search)),
-          IconButton(onPressed: (){}, icon: Icon(Icons.mode_comment)),
+        //title:  Text("appbar",style: TextStyle(color: Colors.green, fontSize: 30),),
+        leading: IconButton(onPressed: () {}, icon: Icon(Icons.arrow_back)),
+        backgroundColor: Colors.white,
 
-        ],
-        bottom: TabBar(
-            indicatorColor:Colors.blue[700],
-            indicatorSize: TabBarIndicatorSize.tab,
-            labelColor: Colors.blue[700],
-            unselectedLabelColor: Colors.black87,
-            tabs: [
-          Tab(
-              icon: Icon(Icons.home_filled,size: 28,)
-          ),
-          Tab(
-              icon: Icon(Icons.ondemand_video_outlined,size:28)
-          ),
-          Tab(
-              icon: Icon(Icons.storefront,size:28)
-          ),
-          Tab(
-              icon: Icon(Icons.videogame_asset_outlined,size:28)
-          ),
 
-          Tab(
-              icon: Icon(Icons.notifications_none,size:28)
-          ),
-          Tab(
-              icon: Icon(Icons.menu,size:28)
-          ),
-        ]),
       ),
 
-          body:
-            TabBarView(children: [
-              Container(color: Colors.white,
-                child: Column(
-                  children: [
-                    Container(
+      body:
+     Column (
+         children: [
+       Container(
+         height: 160,
+         width: double.infinity,
+         color: Colors.white,
+       ),
+       Container(
+         height: 70,
+        width: 70,
+        color: Colors.white,
+        child:
+Image.network("https://img.freepik.com/premium-photo/facebook-logo_1080029-107.jpg?semt=ais_hybrid&w=740")
+     ),
+       Container(
+         height: 100,
+           width: double.infinity,
 
-                      height: 60,
-                      child:
-                        Row(children: [
-                          Container(width:8,),
-                          CircleAvatar(
-                            backgroundImage: NetworkImage("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQRXxfn1j1vKFy8yJeBGl2AS6Dcah-lKgHofg&s"),
-                            child: Icon(Icons.person),
-                            radius:25,
-                          ),
-                          Container(width: 12,),
-                          Container(
-                            margin: EdgeInsets.only(top: 10,bottom: 10),
-                            width: 280,
-                            //color: Colors.green,
-                            child: TextField(
+       ),
+       Container(
+         margin: EdgeInsets.only(left: 25,right: 25),
+         child:
+         TextField(
+           controller: username,
+           decoration: InputDecoration(
+             border: OutlineInputBorder(borderRadius: BorderRadius.circular(12) ),
+             hintText: "Mobile number or email arrdess",
+             hintStyle: TextStyle(color: Colors.grey, fontSize: 17),
+             enabledBorder:
+               OutlineInputBorder(borderRadius: BorderRadius.circular(12),borderSide: BorderSide(color: Colors.grey,width: 1), ),
+           ),
+         ),
+       ),
+       SizedBox(
+         height: 15,
+       ),
+       Container(
+         margin: EdgeInsets.only(left: 25,right: 25),
+         child:
+         TextField(
+           controller: password,
+           decoration: InputDecoration(
+             border: OutlineInputBorder(borderRadius: BorderRadius.circular(12) ),
+             hintText: "Password",
+             hintStyle: TextStyle(color: Colors.grey, fontSize: 17),
+             enabledBorder:
+             OutlineInputBorder(borderRadius: BorderRadius.circular(12),borderSide: BorderSide(color: Colors.grey,width: 1), ),
+           ),
+         ),
 
-                              decoration: InputDecoration(
-                                contentPadding: EdgeInsets.only(top: 10,bottom: 10,left: 23),
-                                  hintText: "What's on your mind?",
-                                  border: OutlineInputBorder(borderRadius: BorderRadius.circular(60),borderSide: BorderSide(color: Colors.grey,width: 0.8) ),
+       ),
 
-
-                              ),
-
-
-
-                            ),
-
-                          ),
-                          Container(width: 17,),
-                          Container(
-                            child: 
-                            Icon(Icons.photo_rounded,color: Colors.green[600]),
-
-                          )
-
-                        ],)
-                    ),
-                    Container(
-                      color: Colors.grey[350],
-                      height: 4,
-                    ),
-
-                    Container(
-                      color: Colors.white,
-                      height: 220,
-                      child: Row(children: [
-
-
-                      ],),
-                    ),
-                    Container(
-                      color: Colors.grey[350],
-                      height: 4,
-                    ),
-
-                  ],
-                ),
-
-              ),
-              Container(color: Colors.black),
-              Container(color: Colors.white),
-              Container(color: Colors.white),
-              Container(color: Colors.white),
-              Container(color: Colors.white),
-
-            ])
+       SizedBox(
+         height: 15,
+       ),
 
 
-    ));
 
+       ElevatedButton(onPressed: () {
+         if (username.text=='user' && password.text=='1234') {
+           //print(username.text);
+           // print(password.text);
+           Navigator.of(context).push(
+               MaterialPageRoute(builder: (context) => homepage(),));
+         }else
+           ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text("Username & password does not match")));
+         
+           
+       } ,
+
+
+           child:
+           Text(" Log in ",style: TextStyle(color: Colors.white),),
+         style: ElevatedButton.styleFrom(backgroundColor:  Colors.blue,
+         minimumSize: Size(365, 45)),
+
+ 
+
+
+       ),
+
+       SizedBox(
+         height: 15,
+       ),
+
+
+       Container(
+        child:
+        Text("Forgotten password?", style: TextStyle(color: Colors.black),),
+
+      ),
+       SizedBox(
+         height: 130,
+       ),
+       ElevatedButton(onPressed: () {} ,
+
+
+         child:
+         Text(" Create new account ",style: TextStyle(color: Colors.lightBlue),),
+         style: ElevatedButton.styleFrom(backgroundColor:  Colors.white, shape: StadiumBorder( side: BorderSide(color: Colors.lightBlue)
+         ),
+             minimumSize: Size(365, 45)),
+
+       ),
+
+
+
+      /* ElevatedButton(
+         onPressed: null, child: Row(),
+       ),*/
+       
+  ]
+
+
+     ) );
   }
 }
 
@@ -149,53 +160,3 @@ class homepage extends StatelessWidget {
 
 
 
-
-//
-//
-// appBar: AppBar(
-// toolbarHeight: 80,
-// backgroundColor: Colors.white,
-// title: Text("facebook",style: TextStyle(color: Colors.indigo,fontSize: 30,fontWeight: FontWeight.bold),),
-// actions: [
-// IconButton(onPressed: (){}, icon: Icon(Icons.add_circle,size: 30,)),
-// IconButton(onPressed: (){}, icon: Icon(Icons.search,size: 30,)),
-// IconButton(onPressed: (){}, icon: Icon(Icons.mark_chat_unread,size: 30,)),
-//
-// ],
-// ),
-// body:Column(
-// children: [
-// Row(
-// children: [
-//
-// CircleAvatar(
-// radius: 30,
-// backgroundImage: NetworkImage("https://www.shutterstock.com/image-photo/blond-hair-girl-taking-photo-260nw-2492842415.jpg"),
-// child: Icon(Icons.person),
-//
-// ),
-// Container(
-// width: 20,
-// ),
-// Container(
-// height: 80,
-// width: 270,
-// // color: Colors.blue,
-// child: TextField(
-// decoration: InputDecoration(
-// // border: Cir
-// ),
-// ),
-// ),
-// Container(
-// height: 80,
-// width: 60,
-// // color: Colors.yellow,
-// child: Icon(Icons.image,color: Colors.green,),
-// ),
-//
-// ],
-// )
-// ],
-// )
-//
